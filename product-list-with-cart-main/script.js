a7260487aaadc3c3046f848ed5806de739147743
mikedeleon9 <mikedeleon9@gmail.com>
2024-11-16 17:32:32 -0500

    finlly added the proper functionlity to each button by removing the class from the SVG and adding it to the circle div

--- product-list-with-cart-main/script.js
+++ product-list-with-cart-main/script.js
@@ -3,7 +3,7 @@ const cartContainer = document.querySelector('.cart-container')
 let numberOfItemsInCart = 0;
 let cartArray = [];
 let buttonClicked = false;
-let orderNumber = 0;
+
 
 fetch('./data.json')
 .then(response => response.json())
@@ -43,27 +43,28 @@ fetch('./data.json')
         if(!cartArray.includes(dessertName)){
             cartArray.push(dessertName);
             numberOfItemsInCart++;
-            buttonClicked = true;
+            let orderNumber = 0;
+            
 
-            if(buttonClicked){
+           
                 button.classList.add('buttonIsClicked');
                 button.innerHTML = `
                 <div class="clickedButton">
-                <div class="circle">
-                <svg class="decreaseOrder" xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2"><path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg>
+                <div class="circle decreaseOrder">
+                <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2"><path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg>
                 </div>
                 <span class="quantity-ordered">${orderNumber}</span>
-                <div class="circle">
-               <svg class="increaseOrder" xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg>
+                <div class="circle increaseOrder">
+               <svg  xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg>
                 </div>
                 </div>
                 `
-            }
+          
 
             //adding event listener to increase/decrease buttons
-            const decreaseButton = document.querySelector('.decreaseOrder');
-            const increaseButton = document.querySelector('.increaseOrder');
-            const quantityOrdered = document.querySelector('.quantity-ordered');
+            const decreaseButton = button.querySelector('.decreaseOrder');
+            const increaseButton = button.querySelector('.increaseOrder');
+            const quantityOrdered = button.querySelector('.quantity-ordered');
            
 
             decreaseButton.addEventListener('click', () => {
