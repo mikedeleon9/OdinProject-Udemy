83203993b691f2d8862bedf86aad7344a8fe09f3
mikedeleon9 <mikedeleon9@gmail.com>
2024-11-26 10:49:31 -0500

    starting modal

--- ecommerce-product-page-main/script.js
+++ ecommerce-product-page-main/script.js
@@ -1,3 +1,4 @@
+//Adding Functionality to cycle through the smaller images
 const images = document.querySelectorAll('.image');
 const mainImage = document.querySelector('.big-image');
 
@@ -10,11 +11,12 @@ images.forEach((image, index) => {
         image.style.outline = "2px solid hsla(26, 100%, 55%, 0.849)";
         image.style.opacity = '0.3';
         
-        mainImage.src = `./images/image-product-${index +1 }.jpg`
-    })
-})
+        mainImage.src = `./images/image-product-${index +1 }.jpg`;
+    });
+});
 
 
+//Adding Event Listener to Plus and Minus Buttons to Add to Cart
 const plusButton = document.querySelector('.plus');
 const minusButton = document.querySelector('.minus');
 const numberOfItems = document.querySelector('.number-of-items');
@@ -29,5 +31,7 @@ minusButton.addEventListener('click', () => {
     if(itemNumbers > 0){
         itemNumbers--;
         numberOfItems.textContent = itemNumbers;
-    }
-})
+    };
+});
+
+//Creating Modal that appears when larger image is clicked
