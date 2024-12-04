728789436b5f53f6b675f820a088a5a8016afce7
mikedeleon9 <mikedeleon9@gmail.com>
2024-12-03 22:31:18 -0500

    added tailwind config file to edit custom backgrounds

---
+++ Easybank Landing Page/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html', // Directly reference index.html
+    './**/*.html',  // Include nested HTML files
+    './**/*.js',    // Include any JS files
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+      'main-background': "url('../images/bg-intro-desktop.svg')",
+      'mobile-background': "url('../images/bg-intro-mobile.svg')"
+      
+      }
+    },
+  },
+  plugins: [],
+};
+
+
