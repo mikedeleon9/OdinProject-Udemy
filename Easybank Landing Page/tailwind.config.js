825eeea1cfd6dd64cded17caac3720a896a94034
mikedeleon9 <mikedeleon9@gmail.com>
2024-12-06 10:27:38 -0500

    finished mobile and 2xl responsiveness part of website, now moving to xl, l and m

--- Easybank Landing Page/tailwind.config.js
+++ Easybank Landing Page/tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         'lightGray': 'hsl(220, 16%, 96%)',
         'navy': 'hsl(233, 26%, 24%)',
         'superLightGray': 'hsl(0, 5%, 98%)',
-        'testGray' : "#eee"
+        'testGray' : "#f9f9f9"
       },
     },
   },
