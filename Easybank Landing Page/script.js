39c594434a0ed43a3d4e2e7bf04237710f71595c
mikedeleon9 <mikedeleon9@gmail.com>
2024-12-06 15:57:17 -0500

    adding javascript

--- Easybank Landing Page/script.js
+++ Easybank Landing Page/script.js
@@ -1,1 +1,8 @@
-const hamburgerIcon = document.getElementById('icon-hamburger')
+const hamburgerIcon = document.getElementById('icon-hamburger');
+const dropDown = document.querySelector('.drop-down');
+
+
+hamburgerIcon.addEventListener('click', () => {
+    dropDown.classList.toggle('hidden');
+    hamburgerIcon.classList.toggle('rotate-90')
+})
