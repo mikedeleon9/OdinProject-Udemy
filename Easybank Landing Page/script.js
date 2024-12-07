5bfd9689da014029db8a610e7a6b9482f69df41d
mikedeleon9 <mikedeleon9@gmail.com>
2024-12-07 10:50:39 -0500

    adding border bottom for each selected menu item

--- Easybank Landing Page/script.js
+++ Easybank Landing Page/script.js
@@ -8,3 +8,15 @@ hamburgerIcon.addEventListener('click', () => {
     dropDown.classList.toggle('show');
     
 })
+
+const menuIcons = document.querySelectorAll('.menu-icon');
+
+menuIcons.forEach(icon => {
+    icon.addEventListener('click', () => {
+        menuIcons.forEach(i => i.classList.remove('menu-border'));
+
+    icon.classList.add('menu-border')
+    }
+    
+    )
+})
