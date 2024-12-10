16fee6c662be48ed80b62fbf5eec77cdc956a933
mikedeleon9 <mikedeleon9@gmail.com>
2024-12-09 21:00:13 -0500

    starting app

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import WelcomePage from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      
+      <WelcomePage />
     </div>
   );
 }
---
+++ quiz-app/src/components/Welcome.js
@@ -0,0 +1,9 @@
+import react from "react"
+
+export default function WelcomePage(){
+    return(
+        <div>
+            <h1>Testing</h1>
+        </div>
+    )
+}
