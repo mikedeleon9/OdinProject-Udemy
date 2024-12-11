8eb08cdd6b99026ad867001ac5eaa85c11ab6aab
mikedeleon9 <mikedeleon9@gmail.com>
2024-12-11 11:48:43 -0500

    working on some questions

--- quiz-app/src/components/Category.js
+++ quiz-app/src/components/Category.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 export default function Category({imgSrc, type}){
+
+
     return(
         <div className="w-2/3">
             <div className="flex items-center justify-start border-2 w-full rounded-3xl gap-4 cursor-pointer" >
