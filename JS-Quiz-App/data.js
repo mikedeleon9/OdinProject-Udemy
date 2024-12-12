8dee47f27f6cab9cb40fd60b8f09d67aa82331ad
mikedeleon9 <mikedeleon9@gmail.com>
2024-12-12 13:19:03 -0500

    working on questions

---
+++ JS-Quiz-App/data.js
@@ -0,0 +1,10 @@
+const SportsQuestions = [
+    {
+        question: "Who holds the most tennis grand slams ever?",
+        options: ["A. Rafael Nadal", "B. Roger Federer", "C. Novak Djokovic", "D. Pete Sampras"]
+    },
+    {
+        question: "Which of these players never played in College?",
+        options: ["A. Shaquille O'neal", "B. LeBron James", "C. Michael Jordan", "D. Magic Johnson"]
+    }
+]
