88228a8c3ac5eb9023303da960b6aa47ba622198
mikedeleon9 <mikedeleon9@gmail.com>
2025-01-04 11:32:48 -0500

    working on bottom panel

--- News-Homepage/tailwind.config.js
+++ News-Homepage/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customOrange: "hsl(35, 77%, 62%)"
+      }
+    },
   },
   plugins: [],
 }
