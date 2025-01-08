7f58d4d090aa5e776b816afed621cc0d8043cf93
mikedeleon9 <mikedeleon9@gmail.com>
2025-01-08 15:07:04 -0500

    retrying panel

--- Agency-Landing-Page/sunnyside-agency-landing-page-main/src/script.js
+++ Agency-Landing-Page/sunnyside-agency-landing-page-main/src/script.js
@@ -1,17 +1,7 @@
 const burgerIcon = document.getElementById('burger-icon');
 const sidePanel = document.getElementById('side-panel')
-const body = document.querySelector('body');
 const header = document.querySelector('header');
 
 burgerIcon.addEventListener('click', () => {
-    sidePanel.classList.toggle('translate-x-full');
-    sidePanel.classList.toggle('translate-x-0');
-  
-    if (!header.classList.contains('w-1/2')) {
-      header.classList.add('w-1/2');
-      header.classList.add('pr-4');
-    } else {
-      header.classList.remove('w-1/2');
-      header.classList.remove('pr-4');
-    }
+
   });
