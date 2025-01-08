e79669f65f5c088a45e9f2fb6c3f2bd247762dc2
mikedeleon9 <mikedeleon9@gmail.com>
2025-01-08 12:12:27 -0500

    working on sidepanel, js

--- Agency-Landing-Page/sunnyside-agency-landing-page-main/src/script.js
+++ Agency-Landing-Page/sunnyside-agency-landing-page-main/src/script.js
@@ -1,6 +1,19 @@
 const burgerIcon = document.getElementById('burger-icon');
+const sidePanel = document.getElementById('side-panel')
 const body = document.querySelector('body');
+const header = document.querySelector('header');
 
 burgerIcon.addEventListener('click', () => {
-    body.classList.toggle('w-3/5')
+   sidePanel.classList.toggle('hidden');
+
+   if(!header.classList.contains('w-1/2')){
+        header.classList.add('w-1/2')
+        header.classList.add('px-4')
+        
+   }
+   else{
+    header.classList.remove('w-1/2')
+    header.classList.remove('px-4')
+   }
+   
 })
