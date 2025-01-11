f9a45d03105b4fcce5c5ec4b2478e79c5d51ab0c
mikedeleon9 <mikedeleon9@gmail.com>
2025-01-11 12:00:27 -0500

    trying to animate side panel

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const arrowIcon = document.querySelector('.arrow');
 const nameTitleDiv = document.getElementById('name-title');
 const profilePicture = document.querySelector('.profile-pic');
 const sidePanel = document.querySelector('.side-panel');
+const navMenuItems = document.querySelectorAll('.nav-menu-item');
 
 let isRotated = false;
 
@@ -10,12 +11,27 @@ arrowIcon.addEventListener('click', () => {
     arrowIcon.style.transform = 'rotate(0deg)';
     profilePicture.style.width = '8rem';
     nameTitleDiv.classList.remove('hidden');
+    
+    
+    navMenuItems.forEach(item => {
+        item.classList.remove('hidden');
+    })
 
   } else {
     arrowIcon.style.transform = 'rotate(180deg)';
     profilePicture.style.width = '3rem';
     nameTitleDiv.classList.add('hidden');
-    sidePanel.classList.remove('w-3/12');
+
+    if(sidePanel.classList.contains('w-3/12')){
+        sidePanel.classList.replace('w-3/12', 'w-0');
+    }
+    
+    
+    
+    navMenuItems.forEach(item => {
+        item.classList.add('hidden');
+    })
+    
   }
   isRotated = !isRotated; // Toggle the state
 
