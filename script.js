d71b0b4c63551c0b97f96fadfbfa1fd994941f44
mikedeleon9 <mikedeleon9@gmail.com>
2025-01-11 11:41:42 -0500

    adding more js functionality

--- script.js
+++ script.js
@@ -8,9 +8,14 @@ let isRotated = false;
 arrowIcon.addEventListener('click', () => {
   if (isRotated) {
     arrowIcon.style.transform = 'rotate(0deg)';
+    profilePicture.style.width = '8rem';
+    nameTitleDiv.classList.remove('hidden');
 
   } else {
     arrowIcon.style.transform = 'rotate(180deg)';
+    profilePicture.style.width = '3rem';
+    nameTitleDiv.classList.add('hidden');
+    sidePanel.classList.remove('w-3/12');
   }
   isRotated = !isRotated; // Toggle the state
 
