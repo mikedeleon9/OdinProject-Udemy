123485f7f5f0f9ea0498ee1aa621eae75ef302ee
mikedeleon9 <mikedeleon9@gmail.com>
2025-01-11 13:14:16 -0500

    adding hovereffects

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const nameTitleDiv = document.getElementById('name-title');
 const profilePicture = document.querySelector('.profile-pic');
 const sidePanel = document.querySelector('.side-panel');
 const navMenuItems = document.querySelectorAll('.nav-menu-item');
+const listIcon = document.querySelectorAll('.list-icon')
 
 let isRotated = false;
 
 arrowIcon.addEventListener('click', () => {
   if (isRotated) {
     arrowIcon.style.transform = 'rotate(0deg)';
-    sidePanel.classList.replace('w-20', 'w-3/12');
+    sidePanel.classList.replace('w-24', 'w-3/12');
    
     
     setTimeout(()=> {
@@ -19,25 +20,36 @@ arrowIcon.addEventListener('click', () => {
             item.classList.remove('hidden');
         })
     }, 300)
+
+  
   
 
   } else {
     arrowIcon.style.transform = 'rotate(180deg)';
-    sidePanel.classList.replace('w-3/12', 'w-20');
+    sidePanel.classList.replace('w-3/12', 'w-24');
 
     setTimeout(()=> {
         nameTitleDiv.classList.add('hidden');
         profilePicture.style.width = '4rem';
         navMenuItems.forEach(item => {
             item.classList.add('hidden');
+            
         })
         
     }, 300)
  
     
+    listIcon.forEach(icon => {
+        icon.addEventListener('mouseover', ()=>{
+            icon.setAttribute('fill', "#9fd3c7")
+        })
+    })
     
-    
-   
+    listIcon.forEach(icon => {
+        icon.addEventListener('mouseout', ()=>{
+            icon.setAttribute('fill', "#5f6368")
+        })
+    })
     
   }
   isRotated = !isRotated; // Toggle the state
