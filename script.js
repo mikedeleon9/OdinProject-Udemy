2ef2ec9d9b74185e0328a77cac6b939ffe500303
mikedeleon9 <mikedeleon9@gmail.com>
2025-01-11 12:20:38 -0500

    adding more animations

--- script.js
+++ script.js
@@ -9,28 +9,35 @@ let isRotated = false;
 arrowIcon.addEventListener('click', () => {
   if (isRotated) {
     arrowIcon.style.transform = 'rotate(0deg)';
-    profilePicture.style.width = '8rem';
-    nameTitleDiv.classList.remove('hidden');
+    sidePanel.classList.replace('w-20', 'w-3/12');
+   
     
-    
-    navMenuItems.forEach(item => {
-        item.classList.remove('hidden');
-    })
+    setTimeout(()=> {
+        profilePicture.style.width = '8rem';
+        nameTitleDiv.classList.remove('hidden');
+        navMenuItems.forEach(item => {
+            item.classList.remove('hidden');
+        })
+    }, 300)
+  
 
   } else {
     arrowIcon.style.transform = 'rotate(180deg)';
-    profilePicture.style.width = '3rem';
-    nameTitleDiv.classList.add('hidden');
+    sidePanel.classList.replace('w-3/12', 'w-20');
 
-    if(sidePanel.classList.contains('w-3/12')){
-        sidePanel.classList.replace('w-3/12', 'w-0');
-    }
+    setTimeout(()=> {
+        nameTitleDiv.classList.add('hidden');
+        profilePicture.style.width = '4rem';
+        navMenuItems.forEach(item => {
+            item.classList.add('hidden');
+        })
+        
+    }, 300)
+ 
     
     
     
-    navMenuItems.forEach(item => {
-        item.classList.add('hidden');
-    })
+   
     
   }
   isRotated = !isRotated; // Toggle the state
