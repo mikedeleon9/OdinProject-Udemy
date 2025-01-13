dc9af7c8115ecb8f9f7af5882a17d209a297eca4
mikedeleon9 <mikedeleon9@gmail.com>
2025-01-13 11:50:14 -0500

    starting again

--- URL-Shortening-LandingPage/url-short-page/src/App.js
+++ URL-Shortening-LandingPage/url-short-page/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './index.css';
 import Header from './components/header'
 
+
 function App() {
   return (
-    <div className="App">
+    <div className="App mx-32">
       <Header></Header>
     </div>
   );
--- URL-Shortening-LandingPage/url-short-page/src/components/header.js
+++ URL-Shortening-LandingPage/url-short-page/src/components/header.js
@@ -1,18 +1,21 @@
 import "../input.css"
-import logo from "../images/logo.svg"
+import logo from '../images/logo.svg'
 
 export default function Header(){
     return(
-        <div>
-            <div className="left-side">
+        <div className="flex justify-between items-center">
+            <div className="left-side flex gap-8">
                 <img src={logo}></img>
-                <ul>
-                    <li className="text-red-500">Features</li>
+                <ul className="flex items-center gap-8">
+                    <li>Features</li>
                     <li>Pricing</li>
                     <li>Resources</li>
                 </ul>
             </div>
-            <div className="right-side"></div>
+            <div className="right-side flex items-center gap-8">
+                <p>Login</p>
+                <button>Sign Up</button>
+            </div>
         </div>
     )
 }
