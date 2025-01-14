f725be9d09a8a1704560eb6a4d8d9a61e6392073
mikedeleon9 <mikedeleon9@gmail.com>
2025-01-14 11:56:00 -0500

    working on bottom panel

--- URL-Shortening-LandingPage/url-short-page/src/components/middleSection.js
+++ URL-Shortening-LandingPage/url-short-page/src/components/middleSection.js
@@ -5,6 +5,21 @@ export default function MiddleSection(){
             <p>Track how your links are performing across the web with an Advanced
                 statistics dashboard.
             </p>
+
+        <div>
+            <div>
+                <h3>Brand Recognition</h3>
+                <p>Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instill confidence in your content.</p>
+            </div>
+            <div>
+                <h3>Detailed Records</h3>
+                <p>Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.</p>
+            </div>
+            <div>
+                <h3>Fully Customizable</h3>
+                <p>Improve brand awareness and content discoverability through customizable links, supercharging audience engagements.</p>
+            </div>
+        </div>
         </div>
     )
 }
